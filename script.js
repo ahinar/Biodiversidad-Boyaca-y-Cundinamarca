3abc32d33021dccd0d90612685b64dfb3da7b38c
Ahinar <ahinar@gmail.com>
2024-03-27 10:54:51 -0500

    creacion de pagina mapa

--- script.js
+++ script.js
@@ -1,84 +1,27 @@
+addEventListener("DOMContentLoaded", () => {
+  const imagenes = [
+    "img/alcaravan.jpg",
+    "img/bromelia.jpg",
+    "img/colibri.jpg",
+    "img/frailejon.jpeg",
+    "img/condor-andes.jpg",
+    "img/magnoliopsidas_boyaca.jpg",
+    "img/oso de anteojos.jpg",
+  ];
 
-window.onload = function () {
-    // Variables
-    const IMAGENES = [
-        'img/colibri.jpg',
-        'img/parque.jpg',
-        'img/palmeras.jpg'
-    ];
-    const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
-    let posicionActual = 0;
-    let $botonRetroceder = document.querySelector('#retroceder');
-    let $botonAvanzar = document.querySelector('#avanzar');
-    let $imagen = document.querySelector('#imagen');
-    let $botonPlay = document.querySelector('#play');
-    let $botonStop = document.querySelector('#stop');
-    let intervalo;
+  let i = 1;
+  const img1 = document.querySelector("#img1");
+  const img2 = document.querySelector("#img2");
+  const progressBar = document.querySelector("#progress-bar");
+  const divIndicadores = document.querySelector("#indicadores");
+  let porcentajeBase = 100 / imagenes.length; //Porcentaje total de la barra del progreso
+  let porcentajeActual = porcentajeBase;
 
-    // Funciones
-
-    /**
-     * Funcion que cambia la foto en la siguiente posicion
-     */
-    function pasarFoto() {
-        if(posicionActual >= IMAGENES.length - 1) {
-            posicionActual = 0;
-        } else {
-            posicionActual++;
-        }
-        renderizarImagen();
-    }
-
-    /**
-     * Funcion que cambia la foto en la anterior posicion
-     */
-    function retrocederFoto() {
-        if(posicionActual <= 0) {
-            posicionActual = IMAGENES.length - 1;
-        } else {
-            posicionActual--;
-        }
-        renderizarImagen();
-    }
-
-    /**
-     * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
-     */
-    function renderizarImagen () {
-        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
-    }
-
-    /**
-     * Activa el autoplay de la imagen
-     */
-    function playIntervalo() {
-        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
-        // Desactivamos los botones de control
-        $botonAvanzar.setAttribute('disabled', true);
-        $botonRetroceder.setAttribute('disabled', true);
-        $botonPlay.setAttribute('disabled', true);
-        $botonStop.removeAttribute('disabled');
-
-    }
-
-    /**
-     * Para el autoplay de la imagen
-     */
-    function stopIntervalo() {
-        clearInterval(intervalo);
-        // Activamos los botones de control
-        $botonAvanzar.removeAttribute('disabled');
-        $botonRetroceder.removeAttribute('disabled');
-        $botonPlay.removeAttribute('disabled');
-        $botonStop.setAttribute('disabled', true);
-    }
-
-    // Eventos
-    $botonAvanzar.addEventListener('click', pasarFoto);
-    $botonRetroceder.addEventListener('click', retrocederFoto);
-    $botonPlay.addEventListener('click', playIntervalo);
-    $botonStop.addEventListener('click', stopIntervalo);
-    // Iniciar
-    renderizarImagen();
-}
-
+  for (let index = 0; index < imagenes.length; index++) {
+    const div = document.createElement("div");
+    div.classList.add("circles")
+    div.id =index;
+    divIndicadores.appendChild(div);
+    
+  }
+});
